024b8ac78868dbde7ade1059341c1fefb7d2bb62
João Silva <joao.figueiredosilva@protonmail.com>
2022-10-02 10:41:49 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
-
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime++;
+    }, 1000);
   }
 
-  getMinutes() {
-    // ... your code goes here
+  getMinutes(){
+    const minutes = parseInt(this.currentTime / 60);
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60;
+    return parseInt(seconds);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } if (value>=10){
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.currentTime);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
   }
 }
